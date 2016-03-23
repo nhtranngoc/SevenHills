9753576299ff08348a58858e2c593abd70714034
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2016-03-23 09:26:41 -0400

    Inital Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Seven Hills Foundation Mockup
+
+##Authors: Nam Tran Ngoc, Mike DiMilia
+
+This is a front-end mockup for Seven Hills foundation's IQP project on developing a database/search engine for searching appropriate Assistive Technology devices. 
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+
+var app = express();
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 80;
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.get('/index', function(req, res) {
+  console.log(req.query);
+  //----------POST STUFF-------------
+  // var body = req.body;
+  // var searchQuery = body.search;
+  // console.log(req.body);
+  //---------------------------------
+
+  res.sendFile(path.join(__dirname, '/search.html'));
+});
+
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, function() {
+  console.log('App is listening on port ' + port);
+});
