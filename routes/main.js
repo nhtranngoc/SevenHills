1e0e18707fad2cc93ab248ad44db5ad680d2dab7
Ivan Melnikov <ivanempire@gmail.com>
2016-03-29 19:10:39 -0400

    Fixed routes

--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.resolve('public/index.html'));
 });
 
 router.get('/random', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/solution.html'));
+    res.sendFile(path.resolve('public/solution.html'));
 });
 
 router.get('/search', function(req, res) {
-    res.sendFile(path.join(__dirname, '/public/search.html'));
+    res.sendFile(path.resolve('public/search.html'));
 });
 
 module.exports = router;
