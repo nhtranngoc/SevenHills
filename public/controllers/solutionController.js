8236c263b9adab6b74e55f878fa9caeb9d3c53f4
thecakebandito <mdimil@gmail.com>
2016-04-11 13:39:44 -0400

    Update solutionController.js

--- public/controllers/solutionController.js
+++ public/controllers/solutionController.js
@@ -1,4 +1,8 @@
 angular.module('sevenHillsApp')
-	.controller('solutionController', function($scope){
-		
+	.controller('solutionController', function($scope, $stateParam){
+	    var solutionID = $stateParams.solutionID;
+	    connection.query('SELECT * from Solutions WHERE solutionID = ?', solutionID, function(err, rows, fields))
+	    {
+	    	console.log(rows);
+	    }
 	})
