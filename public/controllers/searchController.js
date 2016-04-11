e8ffcc3e9b501b85222b824cf401521aae565f21
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2016-04-11 10:09:23 -0400

    Search page working

--- public/controllers/searchController.js
+++ public/controllers/searchController.js
@@ -1,8 +1,9 @@
 angular.module('sevenHillsApp')
 	.controller('searchController', function($scope, sharedInfo){
 		console.log(sharedInfo.getProperty())
-		$scope.searchBar = sharedInfo.getProperty().searchString;
+		$scope.searchString = sharedInfo.getProperty().searchString;
+		$scope.searchBar = $scope.searchString;
 		$scope.results = sharedInfo.getProperty().result;
-		$scope.num = $scope.results.length();
+		$scope.num = $scope.results.length;
 		// $scope.num = 5;
 	})
