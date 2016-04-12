bcd120fcc43d3f44190d46ecc3ce127adf2f626b
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2016-04-12 11:27:43 -0400

    Fixed search bar in search page

--- public/controllers/searchController.js
+++ public/controllers/searchController.js
@@ -1,10 +1,14 @@
 angular.module('sevenHillsApp')
-	.controller('searchController', function($scope, $stateParams, searchResolve){
+	.controller('searchController', function($scope, $stateParams, searchResolve, $state){
   		console.log(searchResolve);
 		console.log($stateParams);
 		$scope.searchString = $stateParams.search;
 		$scope.searchBar = $scope.searchString;
 		$scope.results = searchResolve;
 		$scope.num = $scope.results.length;
+
+		$scope.submitSearch = function(){
+			$state.go('search', {search:$scope.searchBar});
+		}
 		// console.log($scope.results[0])
 	})
