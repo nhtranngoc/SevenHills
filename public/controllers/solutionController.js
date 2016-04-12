054995d20e41df9cb422381aa7d8c74de330c2fa
Nam Tran Ngoc <nhtranngoc@wpi.edu>
2016-04-12 12:40:03 -0400

    Fixed difficulty rating on Solution page

--- public/controllers/solutionController.js
+++ public/controllers/solutionController.js
@@ -2,5 +2,5 @@ angular.module('sevenHillsApp')
 	.controller('solutionController', function($scope, $stateParams, $http, solutionResolve){
 	    console.log(solutionResolve);
 	    $scope.solution = solutionResolve[0];
-
+	    jQuery('.rating').rating('rate', parseInt($scope.solution.Difficulty));
 	})
